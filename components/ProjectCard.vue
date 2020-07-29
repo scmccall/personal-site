7961c8d2c481aee3229229f28c5a696928734f69
Sam McCall <smccall@smccall.com>
2020-07-28 20:15:35 -0400

    Add layout for Project Card Component
    I started the layout for a Project Card Vue component that will serve as
    a default template for each project I have on my page. At the moment
    it's not great to look at- I still need to learn more about Bulma.

---
+++ components/ProjectCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <p>hello</p>
+    <!-- Big container -->
+    <div class="">
+      <div>
+        <h4 class="subtitle is-4">Subtitle</h4>
+      </div>
+      <!-- Left Side Content -->
+      <div class="level-left">
+        <h2 class="title is-1">Title</h2>
+      </div>
+
+      <!-- Right Side Content -->
+      <div class="level-right">
+        <img src="../assets/testImage.jpg" width="300px">
+      </div>
+
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
--- pages/index-example.vue
+++ pages/index-example.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="container">
-    <ProjectCard />
+    <div>
+      <Logo />
+      <h1 class="title">
+        personal-site
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
