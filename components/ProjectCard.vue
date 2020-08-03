0595dcd4b28ab2afbafca91afec70f0ad32385c1
Sam McCall <smccall@smccall.com>
2020-08-03 16:42:07 -0400

    Update visual format for Project Card
    I updated the Project card to include a title, as well as a group
    of tags that will serve as symbols for the skills or framworks used in
    each project.

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -7,10 +7,22 @@
     </div>
     <div class="card-content">
       <div class="content">
-        <p>Content here content here content here content here 
-          content here content here content here content here content here 
-          content here content here content here content here content here 
+        <h1 class="title is-3">
+          Project Name
+        </h1>
+        <p>Here is the project description. Lorem ipsum dolor sit amet, 
+          consectetur adipisicing elit. Tempora hic quam iure ipsa eligendi 
+          dolore asperiores architecto. Eum dolores odit ipsam sapiente 
+          consequatur illum rerum quibusdam, distinctio, sit amet dolorum. 
         </p>
+      </div>
+      <div class="tags">
+        <span class="tag">Tag 1</span>
+        <span class="tag">Tag 2</span>
+        <span class="tag">Tag 3</span>
+        <span class="tag">Tag 4</span>
+        <span class="tag">Tag 5</span>
+        <span class="tag">Tag 6</span>
       </div>
     </div>
     <div class="card-footer">
